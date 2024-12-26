0d78e75dd34c8b8c9a178d11edde28ef98abc5f1
Abhi-GhoGhari <abhighoghari8182@gmail.com>
2024-12-26 12:31:55 +0530

    26/12/2024

--- lib/presentation/view/page/home_page/home_page.dart
+++ lib/presentation/view/page/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glassmorphism_ui/glassmorphism_ui.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,7 +10,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       drawer: Drawer(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.transparent,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -63,33 +64,34 @@ class HomePage extends StatelessWidget {
           width: double.infinity,
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("lib/assets/images/bgimg1.jpg"),
+              image: AssetImage("lib/assets/images/3.jpg"),
               fit: BoxFit.cover,
             ),
           ),
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: ListView(
-              physics: const AlwaysScrollableScrollPhysics(),
+              physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics(),
+              ),
               children: [
                 Container(
                   height: size.height * 0.8,
                   width: size.width * 0.3,
+                  // blur: 10,
                   child: Column(
                     children: [
                       Align(
                         alignment: Alignment.topRight,
-                        child: Container(
-                          height: size.height * 0.3,
-                          width: size.width * 0.6,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: AssetImage("lib/assets/images/abhi.jpg"),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
+                        child: GlassContainer(
+                          height: size.height * 0.35,
+                          width: size.width * 0.65,
+                          blur: 20,
+                          borderRadius: BorderRadius.circular(25),
+                          // color: Colors.white,
+                          // child: const Image(
+                          //   image: AssetImage("lib/assets/images/abhi.jpg"),
+                          // ),
                         ),
                       ),
                       SizedBox(
@@ -121,24 +123,152 @@ class HomePage extends StatelessWidget {
                           "ABHI GHOGHARI'",
                           style: TextStyle(
                             fontSize: 25,
-                            color: Colors.white,
+                            // color: Colors.white,
+                            color: Color(0xffFFEDD8),
+
                             fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
                       SizedBox(
-                        height: size.height * 0.01,
+                        height: size.height * 0.03,
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: GlassContainer(
+                          height: size.height * 0.06,
+                          width: size.width * 0.5,
+                          blur: 25,
+                          borderRadius: BorderRadius.circular(13),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                height: size.height * 0.04,
+                                width: size.width * 0.08,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "lib/assets/logo/github.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: size.height * 0.04,
+                                width: size.width * 0.08,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "lib/assets/logo/linkdin2.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: size.height * 0.04,
+                                width: size.width * 0.08,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "lib/assets/logo/freelancer.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: size.height * 0.04,
+                                width: size.width * 0.08,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "lib/assets/logo/upwork.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.1),
+                      const Text(
+                        "ABOUT ME",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          // color: Colors.white,
+                          color: Color(0xffFFEDD8),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
                 Container(
                   height: size.height * 0.8,
                   width: size.width * 0.3,
-                  color: Colors.white,
+                  // color: Colors.white,
+                  child: Column(
+                    children: [
+                      GlassContainer(
+                        height: size.height * 0.2,
+                        width: double.infinity,
+                        blur: 20,
+                        borderRadius: BorderRadius.circular(15),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "'HI.. , I am flutter application developer ABHI GHOGHARI I have done many flutter application development projects. Like a Chat Functionality App,Gemini AI App, Show a Current City Weather Data App,API based App etc.. Application made by myself'",
+                            style: TextStyle(
+                              fontSize: 16,
+                              // color: Colors.white,
+                              color: Color(0xffFFEDD8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.05),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "1+",
+                              style: TextStyle(
+                                fontSize: 90,
+                                color: Color(0xffFFEDD8),
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "years of experience in this field\nand To take up new projects of\nFlutter application to complete\nthese projects, to keep clients\n"
+                            "happy,\n'Clients are happy, we are happy'",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xffFFEDD8),
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: size.height * 0.1),
+                      GlassContainer(
+                        height: size.height * 0.2,
+                        width: double.infinity,
+                        blur: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
